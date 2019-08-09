ea7c5a2f90241e2bc2ede084958bef67475c3470
Tom <tmbdev@gmail.com>
2019-08-08 23:25:17 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2017-2019 NVIDIA CORPORATION. All rights reserved.
+# This file is part of webloader (see TBD).
+# See the LICENSE file for licensing terms (BSD-style).
+#
+
+from __future__ import print_function
+
+import sys
+import glob
+from distutils.core import setup  # , Extension, Command
+
+if sys.version_info < (3, 6):
+    sys.exit("Python versions less than 3.6 are not supported")
+
+scripts=[]
+
+prereqs = """
+    braceexpand
+    numpy
+    six
+    Pillow
+    simplejson
+    msgpack
+""".split()
+
+setup(
+    name='webdatasets',
+    version='v0.0',
+    author="Thomas Breuel",
+    description="Record sequential storage for deep learning.",
+    packages=["webdataset"],
+    scripts=scripts,
+    install_requires=prereqs,
+)
