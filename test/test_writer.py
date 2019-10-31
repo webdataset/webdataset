bb9ef51b99397b183901fbd91b60a132438be1f5
Tom <tmbdev@gmail.com>
2019-10-30 21:21:19 -0700

    Added gitignore and test_writer

---
+++ test/test_writer.py
@@ -0,0 +1,11 @@
+import webdataset.dataset as wds
+from itertools import islice
+import simplejson
+import sys, os, subprocess
+import PIL
+import io
+import numpy as np
+from webdataset import writer
+
+def test_writer():
+    pass
