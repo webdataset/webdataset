77025f359adb5c79ffad75c055d99f3d15c107ab
Tom Breuel <tbreuel@nvidia.com>
2020-03-09 00:46:52 -0700

    incremented version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = []
 
 setuptools.setup(
     name='webdataset',
-    version="0.1.4",
+    version="0.1.5",
     description="Record sequential storage for deep learning.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
