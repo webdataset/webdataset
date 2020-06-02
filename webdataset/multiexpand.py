62fb3db0ebf930ceea3d3bbca0ba838abbcf1196
Tom <tmbdev+github@gmail.com>
2020-06-02 13:18:56 -0700

    more debug hooks

--- webdataset/multiexpand.py
+++ webdataset/multiexpand.py
@@ -1,15 +1,26 @@
+import sys
+import os
 import multiprocessing as mp
 import queue
 
 
 def _parallel_job(i, f, input_queue, output_queue, extra, kw):
+    verbose = int(os.environ.get("MULTIEXPAND_VERBOSE", 0))
     while True:
         try:
             arg = input_queue.get(False)
         except queue.Empty:
             break
+        if verbose:
+            print(f"MULTIEXPAND JOB [{i}]", f, arg, extra, kw, file=sys.stderr)
+        count = 0
         for sample in f(arg, *extra, **kw):
+            if verbose and count % 1000 == 0:
+                print(
+                    f"MULTIEXPAND SAMPLE [{i}] @{count} {type(sample)}", file=sys.stderr
+                )
             output_queue.put(sample)
+            count += 1
 
 
 def clean_jobs(jobs):
@@ -25,6 +36,7 @@ def clean_jobs(jobs):
 def multiexpand(
     inputs, f, extra=(), kw={}, nworkers=4, input_size=0, output_size=1024, daemon=False
 ):
+    verbose = int(os.environ.get("MULTIEXPAND_VERBOSE", 0))
     input_queue = mp.Queue(input_size)
     output_queue = mp.Queue(output_size)
     jobs = [
@@ -39,11 +51,17 @@ def multiexpand(
         input_queue.put(input)
     for job in jobs:
         job.start()
+    count = 0
     while len(jobs) > 0:
+        if verbose > 9:
+            print("MULTIEXPAND", count, jobs, file=sys.stderr)
         while True:
             try:
-                sample = output_queue.get(False)
+                sample = output_queue.get(True, 0.01)
+                if verbose and count % 1000 == 0:
+                    print("MULTIEXPAND GOT", count, repr(sample)[:50], file=sys.stderr)
             except queue.Empty:
                 break
             yield sample
+            count += 1
         jobs = clean_jobs(jobs)
