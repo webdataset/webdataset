55de1a731bc391b85b33d180048a641dc6dbce2f
Tom <tmbdev+github@gmail.com>
2020-09-08 22:12:01 -0700

    Handle .decode(("special.jpg", my_special_jpg)) now.

--- webdataset/autodecode.py
+++ webdataset/autodecode.py
@@ -97,13 +97,29 @@ imagespecs = {
 
 
 def handle_extension(extensions, f):
+    """Returns a decoder function for the list of extensions.
+
+    Extensions can be a space separated list of extensions.
+    Extensions can contain dots, in which case the corresponding number
+    of extension components must be present in the key given to f.
+    Comparisons are case insensitive.
+
+    Examples:
+
+    handle_extension("jpg jpeg", my_decode_jpg)  # invoked for any file.jpg
+    handle_extension("seg.jpg", special_case_jpg)  # invoked only for file.seg.jpg
+    """
     extensions = extensions.lower().split()
 
     def g(key, data):
-        extension = re.sub(r".*[.]", "", key)
-        if extension.lower() not in extensions:
-            return None
-        return f(data)
+        extension = key.lower().split(".")
+        for target in extensions:
+            target = target.split(".")
+            if len(target) > len(extension):
+                continue
+            if extension[-len(target):] == target:
+                return f(data)
+        return None
 
     return g
 
