21f532a65472954ccdfaeb9a8037a21143a0dbd5
Tom <tmbdev@gmail.com>
2021-02-17 21:47:50 -0800

    shuffle=0 now does no shuffle

--- tasks.py
+++ tasks.py
@@ -26,8 +26,7 @@ def venv(c):
     c.run(f"test -d {VENV} || python3 -m venv {VENV}")
     c.run(f"{ACTIVATE}{PIP} install -r requirements.dev.txt")
     c.run(f"{ACTIVATE}{PIP} install -r requirements.txt")
-    c.run(f"{ACTIVATE} jupyter labextension install @jupyter-widgets/jupyterlab-manager")
-    c.run(f"{ACTIVATE} jupyter labextension install @bokeh/jupyter_bokeh")
+    c.run(f"{ACTIVATE} jupyter labextension install @jupyter-widgets/jupyterlab-manager || true")
     print("done")
 
 
