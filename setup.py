17e429bda2d6fcd7839f11921a53b68923cdf173
Tom <tmbdev+github@gmail.com>
2021-09-05 21:12:55 -0700

    incremented version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = []
 
 setuptools.setup(
     name='webdataset',
-    version="0.1.73",
+    version="0.1.74",
     description="Record sequential storage for deep learning.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
