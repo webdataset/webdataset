363615c5a256703b9e474cc79ba0a6464bf465e8
Tom <tmbdev+github@gmail.com>
2021-11-11 23:46:32 -0800

    fixes, readme

--- webdataset/__init__.py
+++ webdataset/__init__.py
@@ -34,6 +34,10 @@ from .tariterators import (
     tarfile_samples,
     tarfile_to_samples,
 )
+from .cborsiterators import (
+    cbors_to_samples,
+    cbors2_to_samples,
+)
 from .handlers import (
     reraise_exception,
     ignore_and_continue,
@@ -56,12 +60,6 @@ from .autodecode import (
     torch_audio,
     Decoder,
     gzfilter,
-)
-from .tariterators import (
-    url_opener,
-    tar_file_iterator,
-    tar_file_expander,
-    group_by_keys,
 )
 from .filters import (
     transform_with,
