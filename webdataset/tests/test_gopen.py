54d10da1843f38bf1513e8d4715d48f2ba6fd427
Tom <tmbdev@gmail.com>
2022-11-04 12:47:15 -0700

    fixed gopen import

--- webdataset/tests/test_gopen.py
+++ webdataset/tests/test_gopen.py
@@ -4,15 +4,15 @@ from webdataset import gopen
 
 def test_file():
     with tempfile.TemporaryDirectory() as work:
-        with gopen.gopen(f"{work}/temp1", "wb") as stream:
+        with gopen(f"{work}/temp1", "wb") as stream:
             stream.write(b"abc")
-        with gopen.gopen(f"{work}/temp1", "rb") as stream:
+        with gopen(f"{work}/temp1", "rb") as stream:
             assert stream.read() == b"abc"
 
 
 def test_pipe():
     with tempfile.TemporaryDirectory() as work:
-        with gopen.gopen(f"pipe:cat > {work}/temp2", "wb") as stream:
+        with gopen(f"pipe:cat > {work}/temp2", "wb") as stream:
             stream.write(b"abc")
-        with gopen.gopen(f"pipe:cat {work}/temp2", "rb") as stream:
+        with gopen(f"pipe:cat {work}/temp2", "rb") as stream:
             assert stream.read() == b"abc"
