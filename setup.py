5b2ed1c4da34341e002d1174780931272bad37a4
Tom <tmbdev@gmail.com>
2022-12-20 12:42:29 -0800

    added license file to install

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = []
 
 setuptools.setup(
     name="webdataset",
-    version = "0.2.32",
+    version="0.2.32",
     description="Record sequential storage for deep learning.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -38,4 +38,5 @@ setuptools.setup(
     python_requires=">=3.6",
     scripts=scripts,
     install_requires="braceexpand numpy pyyaml".split(),
+    license_files=["LICENSE"],
 )
