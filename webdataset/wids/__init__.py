a90673569f658c280b44727d98a17344489ee23f
Tom <tmbdev@gmail.com>
2023-10-17 16:41:16 -0700

    missing file

---
+++ webdataset/wids/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2017-2019 NVIDIA CORPORATION. All rights reserved.
+# This file is part of the WebDataset library.
+# See the LICENSE file for licensing terms (BSD-style).
+#
+# flake8: noqa
+
+from .wids import ShardListDataset, ShardedSampler
