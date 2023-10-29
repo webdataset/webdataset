4bb1a6b16e8c6c71caca675b792ff0c158d9763c
Tom <tmbdev@gmail.com>
2023-10-28 20:09:23 -0700

    new version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = []
 
 setuptools.setup(
     name="webdataset",
-    version = "0.2.67",
+    version = "0.2.68",
     description="Record sequential storage for deep learning.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
