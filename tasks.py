73e7506d9d5b18bb8fc1dcb4a37e424469d1c7ae
Tom <tmbdev@gmail.com>
2023-12-05 02:23:26 -0800

    updated tasks to use editable pip install

--- tasks.py
+++ tasks.py
@@ -29,6 +29,7 @@ def venv(c):
     # c.run(f"{ACTIVATE}{PIP} install torch==1.8.2+cu102 torchvision==0.9.2+cu102 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html")
     c.run(f"{ACTIVATE}{PIP} install -r requirements.dev.txt")
     c.run(f"{ACTIVATE}{PIP} install -r requirements.txt")
+    c.run(f"{ACTIVATE}{PIP} install -e .")
     print("done")
 
 
