5fd2aa95271fa579bd4d088b99e9ca10cd7f4746
Tom <tmb@sedna>
2024-06-14 02:57:29 -0400

    version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ scripts = []
 
 setuptools.setup(
     name="webdataset",
-    version = "0.2.92",
+    version = "0.2.93",
     description="Record sequential storage for deep learning.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
