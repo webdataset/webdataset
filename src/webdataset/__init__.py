de03327304da44b8153632163315b1e52a3355d6
Tom <tmb@sedna>
2025-02-10 20:10:13 -0500

    Bump version: 0.3.4 → 0.3.5

--- src/webdataset/__init__.py
+++ src/webdataset/__init__.py
@@ -73,4 +73,4 @@ from .tariterators import tarfile_samples, tarfile_to_samples
 from .utils import PipelineStage, repeatedly
 from .writer import ShardWriter, TarWriter, numpy_dumps, torch_dumps
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
